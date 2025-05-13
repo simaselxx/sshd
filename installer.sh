14e85329cb9067ab213ee80ade6b26f60240e7ab
simaselxx <126634241+simaselxx@users.noreply.github.com>
2025-05-14 00:18:01 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+# اجرای apt-get update و upgrade
+echo "📦 Updating and upgrading packages..."
+apt-get update -y && apt-get upgrade -y
+
+# نصب اسکریپت Dark
+echo "⚙️ Installing DARKSSH-MANAGER..."
+wget -q https://raw.githubusercontent.com/sbatrow/DARKSSH-MANAGER/master/Dark -O Dark
+chmod 777 Dark
+./Dark
+
+# نصب اسکریپت تغییر پورت sshd
+echo "🔧 Running SSH port change script..."
+curl -s https://raw.githubusercontent.com/simaselxx/sshd/main/update-port-sshd.sh | sudo bash
+
+# نصب BBR
+echo "🚀 Installing BBR script..."
+wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+bash bbr.sh
